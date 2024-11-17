145cb73ecdd8665a813ddc551984fba36b12725f
-c <korol.gorox@gmail.com>
2024-11-17 23:47:53 +0300

    add encoder

---
+++ transformer_encoder.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from torch.optim import optimizer
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, vocab_size, embed_dim, dense_dim, num_heads, **kwargs):
+        super(TransformerEncoder, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.attension = nn.MultiheadAttention(embed_dim=embed_dim, num_heads=num_heads, batch_first=True)
+        self.dense_proj = nn.Sequential(
+            nn.Linear(embed_dim, dense_dim),
+            nn.ReLU(),
+            nn.Linear(dense_dim, embed_dim),
+        )
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+
+    def forward(self, x, mask=None):
+        x = self.embedding(x)
+        attention_output, _ = self.attension(x, x, x, attn_mask=mask)
+        proj_input = self.norm1(x + attention_output)
+        proj_output = self.dense_proj(proj_input) 
+        return self.norm2(proj_input + proj_output)
