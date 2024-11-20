a10f250e17d83fae374fba981e31da14c4e324b9
-c <korol.gorox@gmail.com>
2024-11-20 20:35:46 +0300

    fix transformer

--- transformer.py
+++ transformer.py
@@ -10,14 +10,14 @@ class Transformer(nn.Module):
         self.encoder = TransformerEncoder(embed_dim, dense_dim, num_heads)
 
         self.global_max_pool = nn.AdaptiveMaxPool1d(1)
-        self.out = nn.Linear(600, 1)
+        self.out = nn.Linear(embed_dim, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, text, mask=None):
         embedded = self.embedding(text)
         encoder_output = self.encoder(embedded, mask)
         # decoder_output = self.decoder(encoder_output, mask)
-        output = self.global_max_pool(encoder_output)
+        output = encoder_output.max(dim=1)[0] # self.global_max_pool(encoder_output)
         output = output.squeeze(-1)  # (batch_size, embed_dim) -> (batch_size, 1)
         output = self.out(output)
         output = self.sigmoid(output)
