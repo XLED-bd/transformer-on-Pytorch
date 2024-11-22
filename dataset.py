f1f79de5559556d7a2035d4dc28cd55816336b6e
-c <korol.gorox@gmail.com>
2024-11-22 21:07:43 +0300

    add AG News dataset

--- dataset.py
+++ dataset.py
@@ -5,6 +5,8 @@ from torchtext.vocab import build_vocab_from_iterator
 import os
 import torch
 
+import pandas as pd
+
 class IMDBDataset(Dataset):
     def __init__(self, root_dir, vocab=None, max_length=600):
         self.max_length = max_length
@@ -52,6 +54,64 @@ class IMDBDataset(Dataset):
             ids = ids[:self.max_length]
         return torch.tensor(ids, dtype=torch.long)
 
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        return self._process_text(text), torch.tensor(label, dtype=torch.long)
+    
+
+class AGDataset(Dataset):
+    def __init__(self, root_dir, vocab=None, max_length=600):
+        self.max_length = max_length
+        self.tokenizer = get_tokenizer('basic_english')
+        
+        self.texts = []
+        self.labels = []
+
+        df_label = pd.read_csv(root_dir, usecols=["Class Index"])["Class Index"].tolist()
+        df_text = pd.read_csv(root_dir, usecols=["Title", "Description"])
+
+        for i in range(0, len(df_text)):
+            self.texts.append(df_text["Title"][i].replace('"', " ").replace("\\", " ") + " " + df_text["Description"][i].replace('"', " ").replace("\\", " "))
+
+            self.labels.append([1, 0, 0, 0] if df_label[i] == 1 else
+                                [0, 1, 0, 0] if df_label[i] == 2 else
+                                  [0, 0, 1, 0] if df_label[i] == 3 else
+                                  [0, 0, 0, 1])
+
+
+        if vocab is None:
+            self.vocab = self._create_vocab()
+        else:
+            self.vocab = vocab
+
+
+    def _create_vocab(self, max_tokens=30000):
+        def yield_tokens() -> Iterable[List[str]]:
+            for text in self.texts:
+                yield self.tokenizer(text)
+        
+        vocab = build_vocab_from_iterator(
+            yield_tokens(),
+            specials=['<pad>', '<unk>'],
+            max_tokens=max_tokens - 2 
+        )
+        vocab.set_default_index(vocab['<unk>'])
+        return vocab
+
+        
+    def _process_text(self, text: str) -> torch.Tensor:
+        tokens = self.tokenizer(text)
+        ids = [self.vocab[token] for token in tokens]
+        if len(ids) < self.max_length:
+            ids = ids + [self.vocab['<pad>']] * (self.max_length - len(ids))
+        else:
+            ids = ids[:self.max_length]
+        return torch.tensor(ids, dtype=torch.long)
+    
     def __len__(self):
         return len(self.texts)
 
--- utils.py
+++ utils.py
@@ -1,11 +1,23 @@
 import os
 from torchtext.vocab import build_vocab_from_iterator
-from dataset import IMDBDataset
+from dataset import IMDBDataset, AGDataset
 from torch.utils.data import DataLoader
 
 
 def create_imdb_dataloader(train_dir, batch_size=32):
     train_dataset = IMDBDataset(train_dir)
+    
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+    
+    return train_dataloader, train_dataset.vocab
+
+
+def create_ag_dataloader(train_dir, batch_size=32):
+    train_dataset = AGDataset(train_dir)
     
     train_dataloader = DataLoader(
         train_dataset,
